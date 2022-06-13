6728a5e420f241753fb25e8524acda779e6a0c78
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-06-13 12:34:42 +0300

    Integrated feature: config files are stored in unified dot-dir (~/.CleanEmon) and managed via dotfiles.py module

--- CleanEmonCore/__init__.py
+++ CleanEmonCore/__init__.py
@@ -1,23 +1,16 @@
 import os
 
-_DATA_DIR_NAME = "data"
+from .dotfiles import get_dotfile
+
 _CONFIG_FILENAME = "db.cfg"
 
 PACKAGE_DIR = os.path.dirname(__file__)
-DATA_DIR = os.path.join(PACKAGE_DIR, _DATA_DIR_NAME)
-
-# Ensure that the `$PACKAGE/data` dir exists
-if not os.path.isdir(DATA_DIR):
-    os.mkdir(DATA_DIR)
 
 # Check if config-file lies in directory of execution
 # If not, consider working with the global one
 if os.path.exists(_CONFIG_FILENAME):
     CONFIG_FILE = os.path.abspath(_CONFIG_FILENAME)
 else:
-    CONFIG_FILE = os.path.join(DATA_DIR, _CONFIG_FILENAME)
-
-    # If there is no global file, generate a new one
-    if not os.path.isfile(CONFIG_FILE):
-        from .setup_scripts import generate_config
-        generate_config(CONFIG_FILE)
+    # Retrieve the config file from the dot-dir or generate a new one
+    from .setup_scripts import generate_config
+    CONFIG_FILE = get_dotfile(_CONFIG_FILENAME, generate_config)

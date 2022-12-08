3ad5faa31da86d0928dc9f07210924fe240903bc
Konstantinos Mazgaltzidis <58148103+konmaz@users.noreply.github.com>
2022-12-08 21:20:33 +0200

    Fixed a sneaky bug in schema

--- CleanEmonCore/json_utils/schemas.py
+++ CleanEmonCore/json_utils/schemas.py
@@ -1,20 +1,18 @@
 schema_devices = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "type": "object",
-    "properties": {
-        "devices": {
-            "type": "array",
-            "minItems": 1,
-            "uniqueItems": True,
-            "items": [
-                {
-                    "type": "string",
-                    "minLength": 1
-                }
-            ]
-        }
-    },
-    "required": [
-        "devices"
-    ]
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "devices": {
+      "type": "array",
+      "minItems": 1,
+      "uniqueItems": True,
+      "items": {
+        "type": "string",
+        "minLength": 1
+      }
+    }
+  },
+  "required": [
+    "devices"
+  ]
 }

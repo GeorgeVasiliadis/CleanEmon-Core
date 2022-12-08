4d069e19bc4c437981800d9ae9e20a9e9c4cacf4
Konstantinos Mazgaltzidis <58148103+konmaz@users.noreply.github.com>
2022-12-08 21:37:05 +0200

    Rename parameter of verify_json to avoid shadowing

--- CleanEmonCore/json_utils/json_utils.py
+++ CleanEmonCore/json_utils/json_utils.py
@@ -3,13 +3,14 @@ import json
 from jsonschema import validate
 
 
-def verify_json(json: dict, schema: dict) -> dict:
+def verify_json(json_instance: dict, schema: dict) -> dict:
     """
     If the json doesn't follow the schema an exception will be raised.
     Else the json will be returned.
     """
-    validate(instance=json, schema=schema)
-    return json
+
+    validate(instance=json_instance, schema=schema)
+    return json_instance
 
 
 def read_json_file(path):
